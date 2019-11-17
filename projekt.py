f4b6488852e1db0f58500cec6f5bf3d73359fbb1
DanikTL <danik4tl@gmail.com>
2019-11-17 02:02:28 +0100

    big big update

--- projekt.py
+++ projekt.py
@@ -1,66 +1,145 @@
-import sys
+import sys, time, math
 
 import numpy as np
 from matplotlib import pylab as plt
+from matplotlib import pyplot as pplt
+import matplotlib.patches as mpatches
 from scipy.signal import medfilt
 from skimage import io, data, color, img_as_ubyte
 from skimage.transform import resize
-from skimage.filters import threshold_yen, gaussian, median
+from skimage.filters import threshold_yen, gaussian, median, threshold_li, threshold_isodata, threshold_otsu, threshold_mean
 from skimage.feature import canny
-from skimage.measure import find_contours
+from skimage.measure import find_contours, regionprops, moments
 from skimage.morphology import remove_small_objects, binary_opening, remove_small_holes, dilation, disk
 from skimage.transform import hough_ellipse, hough_circle, hough_circle_peaks
 from skimage.draw import ellipse_perimeter, circle_perimeter
+from skimage.color import rgb2hsv
+from skimage.morphology import closing, square
+
+from skimage.segmentation import clear_border
+from skimage.measure import label, regionprops
+from skimage.morphology import closing, square
+from skimage.color import label2rgb
+
+HEIGHT = 250
+WIDTH = 250
+
+font = {'family': 'serif',
+        'color': 'darkred',
+        'weight': 'normal',
+        'size': 16,
+        }
 
 
 def im_resize(img):
-    return resize(img, (250, 250))
+    return resize(img, (HEIGHT, WIDTH))
 
 
-def main():
-    for n in range(1, 4):
+def get_contrasted(img, perc):
+    # MIN = np.percentile(img, perc)
+    # MAX = np.percentile(img, 100 - perc)
+    # norm = (img - MIN) / (MAX - MIN)
+    # norm[norm[:, :] > 1] = 1
+    # norm[norm[:, :] < 0] = 0
+
+    # OR
+
+    gamma_coef = 2.5
+    norm = img**gamma_coef
+
+    return norm
+
+
+def main(level='easy', img_number=3):
+    for n in range(1, 1 + img_number):
         # read image from input and make edge detection to find ellipse next
-        im = im_resize(io.imread(f'examples/easy/coin{n}.jpg', as_gray=True))
-        im = gaussian(im, sigma=2)
-        thresh = threshold_yen(im)
+        im = im_resize(io.imread(f'examples/{level}/coin{n}.jpg', as_gray=True))
+
+        # plt.imshow(1 - im, cmap='binary')
+        # plt.title("cleared1")
+        # plt.show()
+        # time.sleep(1)
+
+        im = median(im, disk(3))
+
+        # add contrast
+        im = get_contrasted(im, 15)
+
+        # plt.imshow(1-im, cmap='binary')
+        # plt.title("contrasted")
+        # plt.show()
+        # time.sleep(1)
+
+        im = median(im, disk(3))
+
+        thresh = threshold_isodata(im)
         binary = im > thresh
+
+        # plt.imshow(1-binary, cmap='binary')
+        # plt.title("Afterthrashed")
+        # plt.show()
+        # time.sleep(1)
+
         binary_cleaned = remove_small_holes(binary, 1000)
         binary_cleaned = remove_small_objects(binary_cleaned, 1000)
+
+        # plt.imshow(1-binary_cleaned, cmap='binary')
+        # plt.title("Cleaned")
+        # plt.show()
+        # time.sleep(1)
 
         binary_smooth = medfilt(binary_cleaned, 3)
         binary_smooth = dilation(canny(binary_smooth))
 
-        # show detected edges for de
-        plt.imshow(1-binary_smooth, cmap='binary')
+        # plt.imshow(1-binary_smooth, cmap='binary')
+        # plt.title("Smooth")
+        # plt.show()
+        # time.sleep(1)
+
+        labels = label(binary_smooth)
+
+        im_rgb = im_resize(io.imread(f'examples/{level}/coin{n}.jpg'))
+
+        fig, ax = plt.subplots(figsize=(10, 6))
+
+        ax.imshow(im_rgb)
+
+        for region in regionprops(labels):
+
+            # th = 4 * math.pi * region.area / (region.perimeter**2)
+            # print(th, region.area, region.perimeter)
+            # take regions with large enough areas
+            if region.area >= 50:
+                # draw rectangle around segmented coins
+                minr, minc, maxr, maxc = region.bbox
+
+                rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
+                                          fill=False, edgecolor='green', linewidth=2)
+                center = [int(rect.get_x() + rect.get_width() / 2), int(rect.get_y() + rect.get_height() / 2), ]
+                # m = min([rect.get_width(), rect.get_height()])
+                col = np.average(rgb2hsv(im_rgb)[center[1] - 5:center[1] + 5, center[0] - 5:center[0] + 5, :], axis=(0, 1))
+                h, s, v = col[0], col[1], col[2]
+
+                if v < .1:
+                    print("Black")
+                    continue
+                # detect coin's color
+                if .7 > s > .3:
+                    txt = 'Golden'
+                elif .3 > s > 0:
+                    txt = 'Silver'
+                else:
+                    continue
+                ax.add_patch(rect)
+                ax.text(rect.get_x(), rect.get_y() - 5, txt, fontdict=font)
         plt.show()
 
-        hough_radii = np.arange(15, 50, 2)
-        hough_res = hough_circle(binary_smooth, hough_radii)
 
-        # Select the most prominent 3 circles
-        accums, cx, cy, radii = hough_circle_peaks(hough_res, hough_radii,
-                                                   total_num_peaks=3)
-        print(hough_res, hough_res.shape)
-
-
-        im_rgb = im_resize(io.imread(f'examples/easy/coin{n}.jpg'))
-        if len(hough_res) > 0:
-            for center_y, center_x, radius in zip(cy, cx, radii):
-                circy, circx = circle_perimeter(center_y, center_x, radius,
-                                                shape=im_rgb.shape)
-                im_rgb[circy, circx] = (1, 0, 0)
-
-            # best = list(result[-1])
-            # yc, xc, a, b = [int(round(x)) for x in best[1:5]]
-            # orientation = best[5]
-            #
-            # cy, cx = ellipse_perimeter(yc, xc, a, b, orientation)
-            plt.imshow(im_rgb)
-
-            plt.show()
-        else:
-            print("no circle found")
+def mainloop():
+    # main('easy', 3)
+    main("medium", 2)
+    # main("hard", 2)
 
 
 if __name__ == '__main__':
-    main()
+    mainloop()
