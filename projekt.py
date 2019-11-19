87dfe9689efe59323df4e1b1799d418bde97424b
DanikTL <danik4tl@gmail.com>
2019-11-19 12:44:00 +0100

    5 zlotych

--- projekt.py
+++ projekt.py
@@ -10,6 +10,7 @@ from skimage.filters import threshold_yen, gaussian, median, threshold_li, thres
 from skimage.feature import canny
 from skimage.morphology import remove_small_objects, binary_opening, remove_small_holes, dilation, disk
 from skimage.color import rgb2hsv
+from skimage import exposure
 
 from skimage.measure import label, regionprops
 
@@ -29,24 +30,60 @@ def im_resize(img):
 
 
 def get_contrasted(img, perc):
-    # MIN = np.percentile(img, perc)
-    # MAX = np.percentile(img, 100 - perc)
+    # MIN = 180/255
+    # MAX = 1
     # norm = (img - MIN) / (MAX - MIN)
     # norm[norm[:, :] > 1] = 1
     # norm[norm[:, :] < 0] = 0
 
     # OR
-
+    #
     gamma_coef = 2.5
     norm = img**gamma_coef
 
     return norm
 
 
+def check_color(h, s, v):
+    txt = None
+    if v < .1:
+        print('Too dark')
+    # detect coin's color
+    if .7 > s > .3 and 55/360 > h > 20/360:
+        txt = 'Golden'
+    elif .3 > s > 0:
+        txt = 'Silver'
+    else:
+        print(h*360)
+    return txt
+
+
+def get_average_hsv(segment):
+    col = np.average(rgb2hsv(segment), axis=(0, 1))
+    h, s, v = col[0], col[1], col[2]
+    return h, s, v
+
+
+def check_if_five(coin):
+    # -- diameter for golden kernel is 16, whole coin diameter is 24 (1/3)
+    r = coin.shape[0]//8
+    rf = r//3
+    c = coin.shape[1]//2
+    coin_check_point = coin[rf:rf+rf, c-rf:c+rf, :]
+    h, s, v = get_average_hsv(coin_check_point)
+    if check_color(h, s, v) == "Silver":
+        return True
+    return False
+
+
 def main(level='easy', img_number=3):
     for n in range(1, 1 + img_number):
-        # read image from input and make edge detection to find ellipse next
+        # if n != 2:
+        #     continue
+        # -- read image from input and make edge detection to find ellipse next
         im = im_resize(io.imread(f'examples/{level}/coin{n}.jpg', as_gray=True))
+
+        im = exposure.rescale_intensity(im)
 
         # plt.imshow(1 - im, cmap='binary')
         # plt.title("cleared1")
@@ -55,7 +92,7 @@ def main(level='easy', img_number=3):
 
         im = median(im, disk(3))
 
-        # add contrast
+        # -- add contrast
         im = get_contrasted(im, 15)
 
         # plt.imshow(1-im, cmap='binary')
@@ -93,7 +130,38 @@ def main(level='easy', img_number=3):
 
         im_rgb = im_resize(io.imread(f'examples/{level}/coin{n}.jpg'))
 
+        # plt.imshow(im_rgb)
+        # plt.title("img")
+        # plt.show()
+        # time.sleep(1)
+
+
         fig, ax = plt.subplots(figsize=(6, 6))
+
+        # im_hsv = rgb2hsv(im_rgb)
+
+        # print(np.max(im_hsv[:, :, 1]))
+
+        # mask_golden = ((im_hsv[:, :, 1] > .3) & (im_hsv[:, :, 1] > .7))
+        # mask_golden = remove_small_holes(mask_golden, 1000)
+        # mask_golden = remove_small_objects(mask_golden, 1000)
+        #
+        #
+        # plt.imshow(mask_golden, cmap='gray')
+        # plt.title("golden")
+        # plt.show()
+        # time.sleep(1)
+        #
+        # mask_silver = im_hsv[:, :, 1] < .3
+        # mask_silver = remove_small_holes(mask_silver, 1000)
+        # mask_sliver = remove_small_objects(mask_silver, 1000)
+        #
+        # plt.imshow(mask_sliver, cmap='gray')
+        # plt.title("silver")
+        # plt.show()
+        # time.sleep(1)
+
+        # continue
 
         ax.imshow(im_rgb)
 
@@ -109,30 +177,24 @@ def main(level='easy', img_number=3):
                 rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
                                           fill=False, edgecolor='green', linewidth=2)
                 center = [int(rect.get_x() + rect.get_width() / 2), int(rect.get_y() + rect.get_height() / 2), ]
-                # m = min([rect.get_width(), rect.get_height()])
-                col = np.average(rgb2hsv(im_rgb)[center[1] - 5:center[1] + 5, center[0] - 5:center[0] + 5, :], axis=(0, 1))
-                h, s, v = col[0], col[1], col[2]
 
-                if v < .1:
-                    print("Black")
+                h, s, v = get_average_hsv(im_rgb[center[1] - 5:center[1] + 5, center[0] - 5:center[0] + 5, :])
+                coin_center_color = check_color(h, s, v)
+                if isinstance(coin_center_color, type(None)):
                     continue
-                # detect coin's color
-                if .7 > s > .3:
-                    txt = 'Golden'
-                elif .3 > s > 0:
-                    txt = 'Silver'
-                else:
-                    continue
+                if coin_center_color == "Golden":
+                    five = check_if_five(im_rgb[minr:maxr, minc:maxc, :])
+                    if five:
+                        coin_center_color = "Pięć złotych"
                 ax.add_patch(rect)
-                ax.text(rect.get_x(), rect.get_y() - 5, txt, fontdict=font)
-                ax.set_xticks([])
-                ax.set_yticks([])
+                ax.text(rect.get_x(), rect.get_y() - 5, coin_center_color, fontdict=font)
+                ax.axis('off')
         plt.show()
 
 
 def mainloop():
-    # main('easy', 3)
-    main("medium", 2)
+    # main('easy', 4)
+    main("medium", 4)
     # main("hard", 2)
 
 
