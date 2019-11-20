518b879785e670a8486852c318b0ac39b2e601eb
danikTL <37304160+daniktl@users.noreply.github.com>
2019-11-20 15:04:48 +0100

    New easy pictures. 5 and 2 zlotych recognition. adaptive resize. all images from folders

--- projekt.py
+++ projekt.py
@@ -1,4 +1,4 @@
-import sys, time, math
+import sys, time, math, os
 
 import numpy as np
 from matplotlib import pylab as plt
@@ -6,27 +6,26 @@ import matplotlib.patches as mpatches
 from scipy.signal import medfilt
 from skimage import io
 from skimage.transform import resize
-from skimage.filters import threshold_yen, gaussian, median, threshold_li, threshold_isodata, threshold_otsu, threshold_mean
+from skimage.filters import sobel, threshold_yen, gaussian, median, threshold_li, threshold_isodata, threshold_otsu, threshold_mean
 from skimage.feature import canny
-from skimage.morphology import remove_small_objects, binary_opening, remove_small_holes, dilation, disk
+from skimage.morphology import watershed, remove_small_objects, binary_opening, remove_small_holes, dilation, disk, binary_closing, erosion
 from skimage.color import rgb2hsv
 from skimage import exposure
+from scipy import ndimage as ndi
 
 from skimage.measure import label, regionprops
 
-
-HEIGHT = 250
-WIDTH = 250
+MAX_WIDTH = 500
 
 font = {'family': 'serif',
         'color': 'darkred',
         'weight': 'normal',
-        'size': 16,
+        'size': 12,
         }
 
 
 def im_resize(img):
-    return resize(img, (HEIGHT, WIDTH))
+    return resize(img, (int(img.shape[0]*(MAX_WIDTH/img.shape[1])), MAX_WIDTH))
 
 
 def get_contrasted(img, perc):
@@ -38,7 +37,7 @@ def get_contrasted(img, perc):
 
     # OR
     #
-    gamma_coef = 2.5
+    gamma_coef = 2
     norm = img**gamma_coef
 
     return norm
@@ -49,12 +48,12 @@ def check_color(h, s, v):
     if v < .1:
         print('Too dark')
     # detect coin's color
-    if .7 > s > .3 and 55/360 > h > 20/360:
+    if .7 > s > .3 and 65/360 > h > 20/360:
         txt = 'Golden'
-    elif .3 > s > 0:
+    elif .3 >= s > 0:
         txt = 'Silver'
     else:
-        print(h*360)
+        print(f"Unknown color, hue: {h*360}, saturation: {s}")
     return txt
 
 
@@ -66,8 +65,8 @@ def get_average_hsv(segment):
 
 def check_if_five(coin):
     # -- diameter for golden kernel is 16, whole coin diameter is 24 (1/3)
-    r = coin.shape[0]//8
-    rf = r//3
+    r = coin.shape[0]//6
+    rf = r//2
     c = coin.shape[1]//2
     coin_check_point = coin[rf:rf+rf, c-rf:c+rf, :]
     h, s, v = get_average_hsv(coin_check_point)
@@ -76,92 +75,100 @@ def check_if_five(coin):
     return False
 
 
-def main(level='easy', img_number=3):
-    for n in range(1, 1 + img_number):
+def check_if_two(coin):
+    r = coin.shape[0] // 5
+    rf = r // 3
+    c = coin.shape[1] // 2
+    coin_check_point = coin[rf:rf + rf, c - rf:c + rf, :]
+    print(coin_check_point.shape, r, rf, c)
+    h, s, v = get_average_hsv(coin_check_point)
+    if check_color(h, s, v) == "Golden":
+        return True
+    return False
+
+
+def main(level='easy'):
+    for n, file in enumerate(os.listdir(f'examples/{level}')):
+        if not any([extension in file.lower() for extension in ['jpg', 'jpeg', 'png']]):
+            continue
         # if n != 2:
         #     continue
         # -- read image from input and make edge detection to find ellipse next
-        im = im_resize(io.imread(f'examples/{level}/coin{n}.jpg', as_gray=True))
+        im = im_resize(io.imread(f'examples/{level}/{file}', as_gray=True))
 
-        im = exposure.rescale_intensity(im)
-
-        # plt.imshow(1 - im, cmap='binary')
-        # plt.title("cleared1")
+        # plt.imshow(im, cmap='gray')
+        # plt.title("image")
         # plt.show()
         # time.sleep(1)
 
-        im = median(im, disk(3))
+        # im = median(im, disk(3))
 
         # -- add contrast
         im = get_contrasted(im, 15)
 
-        # plt.imshow(1-im, cmap='binary')
+        # plt.imshow(im, cmap='gray')
         # plt.title("contrasted")
         # plt.show()
         # time.sleep(1)
 
-        im = median(im, disk(3))
-
-        thresh = threshold_isodata(im)
-        binary = im > thresh
-
-        # plt.imshow(1-binary, cmap='binary')
+        # im = median(im, disk(2))
+        #
+        # thresh = threshold_yen(im)
+        # binary = im > thresh
+        #
+        # plt.imshow(binary, cmap='gray')
         # plt.title("Afterthrashed")
         # plt.show()
         # time.sleep(1)
-
-        binary_cleaned = remove_small_holes(binary, 1000)
-        binary_cleaned = remove_small_objects(binary_cleaned, 1000)
-
+        #
+        # binary_cleaned = remove_small_holes(binary, 100)
+        # binary_cleaned = remove_small_objects(binary_cleaned, 100)
+        #
         # plt.imshow(1-binary_cleaned, cmap='binary')
         # plt.title("Cleaned")
         # plt.show()
         # time.sleep(1)
-
-        binary_smooth = medfilt(binary_cleaned, 3)
-        edges = dilation(canny(binary_smooth))
-
-        # plt.imshow(1-binary_smooth, cmap='binary')
+        #
+        # binary_smooth = medfilt(binary_cleaned, 3)
+        # edges = dilation(canny(binary_smooth))
+        #
+        # plt.imshow(edges, cmap='gray')
         # plt.title("Smooth")
         # plt.show()
         # time.sleep(1)
 
-        labels = label(edges)
+        im = binary_closing(canny(im))
 
-        im_rgb = im_resize(io.imread(f'examples/{level}/coin{n}.jpg'))
+        # plt.imshow(im, cmap='gray')
+        # plt.title("Canny")
+        # plt.show()
+        # time.sleep(1)
+
+        objects = binary_closing(ndi.binary_fill_holes(im))
+        # objects = remove_small_holes(objects, 500)
+
+        # plt.imshow(objects, cmap='gray')
+        # plt.title("Objects small holes")
+        # plt.show()
+        # time.sleep(1)
+
+        objects = remove_small_objects(objects, 500)
+
+        # plt.imshow(objects_cleaned, cmap='gray')
+        # plt.title("Objects")
+        # plt.show()
+        # time.sleep(1)
+
+        labels = label(objects)
+
+        im_rgb = im_resize(io.imread(f'examples/{level}/{file}'))
 
         # plt.imshow(im_rgb)
         # plt.title("img")
         # plt.show()
         # time.sleep(1)
 
-
         fig, ax = plt.subplots(figsize=(6, 6))
-
-        # im_hsv = rgb2hsv(im_rgb)
-
-        # print(np.max(im_hsv[:, :, 1]))
-
-        # mask_golden = ((im_hsv[:, :, 1] > .3) & (im_hsv[:, :, 1] > .7))
-        # mask_golden = remove_small_holes(mask_golden, 1000)
-        # mask_golden = remove_small_objects(mask_golden, 1000)
-        #
-        #
-        # plt.imshow(mask_golden, cmap='gray')
-        # plt.title("golden")
-        # plt.show()
-        # time.sleep(1)
-        #
-        # mask_silver = im_hsv[:, :, 1] < .3
-        # mask_silver = remove_small_holes(mask_silver, 1000)
-        # mask_sliver = remove_small_objects(mask_silver, 1000)
-        #
-        # plt.imshow(mask_sliver, cmap='gray')
-        # plt.title("silver")
-        # plt.show()
-        # time.sleep(1)
-
-        # continue
 
         ax.imshow(im_rgb)
 
@@ -170,7 +177,7 @@ def main(level='easy', img_number=3):
             # th = 4 * math.pi * region.area / (region.perimeter**2)
             # print(th, region.area, region.perimeter)
             # take regions with large enough areas
-            if region.area >= 50:
+            if region.area >= 40:
                 # draw rectangle around segmented coins
                 minr, minc, maxr, maxc = region.bbox
 
@@ -186,16 +193,20 @@ def main(level='easy', img_number=3):
                     five = check_if_five(im_rgb[minr:maxr, minc:maxc, :])
                     if five:
                         coin_center_color = "Pięć złotych"
+                elif coin_center_color == "Silver":
+                    two = check_if_two(im_rgb[minr:maxr, minc:maxc, :])
+                    if two:
+                        coin_center_color = "Dwa złote"
                 ax.add_patch(rect)
-                ax.text(rect.get_x(), rect.get_y() - 5, coin_center_color, fontdict=font)
+                ax.text(rect.get_x(), rect.get_y() - 2, coin_center_color, fontdict=font)
                 ax.axis('off')
         plt.show()
 
 
 def mainloop():
-    # main('easy', 4)
-    main("medium", 4)
-    # main("hard", 2)
+    main('easy')
+    # main("medium")
+    # main("hard")
 
 
 if __name__ == '__main__':
